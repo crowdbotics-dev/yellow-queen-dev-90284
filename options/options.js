162d80bbdd0ae7c44fca7db3d7ade74409ee0379
Crowdbotics <engineering@crowdbotics.com>
2023-06-10 23:14:35 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "yellow-queen-dev-90284",
+  url: "https://yellow-queen-dev-90284.botics.co",
+  api: "https://yellow-queen-dev-90284.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
